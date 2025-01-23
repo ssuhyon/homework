8b5a25cd6369cf88a7a404d9b865764fdae2246d
ssuhyon <josuhyeon@icloud.com>
2025-01-23 10:58:06 +0900

    update

--- january/1/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignUpView, UserUpdateView, MyPageView
-
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('mypage/', MyPageView.as_view(), name='mypage'),
-    path('mypage/edit/', UserUpdateView.as_view(), name='edit_profile'),
-]
