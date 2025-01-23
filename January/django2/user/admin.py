63dea1a5c7ad03408dc0ea7313f9b8777988f4bb
josuhyeon <ssuhyon.jo@gmail.com>
2025-01-23 11:59:25 +0900

    Revert "Homework"

--- January/django2/user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('username', 'email', 'phone_number', 'is_staff', 'is_superuser')
-
-admin.site.register(CustomUser, CustomUserAdmin)
--- January/django2/user/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CustomUser
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'profile_image', 'phone_number', 'address', 'is_phone_verified']
